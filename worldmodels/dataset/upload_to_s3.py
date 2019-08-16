617e120a92294776058d2a9e2333e5674a1bff68
Adam Green <adam.green@adgefficiency.com>
2019-08-16 10:06:49 +0200

    sync

--- worldmodels/dataset/upload_to_s3.py
+++ worldmodels/dataset/upload_to_s3.py
@@ -14,18 +14,21 @@ def list_local_records(record_dir, incl):
     print('finding objects on local that contain {} in {}'.format(incl, record_dir))
     record_dir = os.path.join(results_dir, record_dir)
     files = os.listdir(record_dir)
-    return sorted([os.path.join(record_dir, f) for f in files if incl in f])
+    files = sorted([os.path.join(record_dir, f) for f in files if incl in f])
+    print('found {} files'.format(len(files)))
+    return files
 
 
 class S3:
     """ interface for AWS S3 """
     def __init__(self, bucket='world-models'):
+        self.bucket_name = bucket
+
         self.client = boto3.client(
             's3',
             region_name='eu-central-1',
             config=Config(signature_version=UNSIGNED)
         )
-        self.bucket_name = bucket
 
     def post(self, fname, key=None):
         if key is None:
@@ -39,11 +42,11 @@ class S3:
 
     def list_all_objects(self, contains):
         print('finding objects on S3 that contain {}'.format(contains))
-
         s3 = boto3.resource('s3')
         bucket = s3.Bucket('world-models')
         objs = bucket.objects.all()
         objs = [o for o in objs if contains in o.key]
+        print('found {} objects'.format(objs))
         return sorted(
             ['s3://{}/{}'.format(self.bucket_name, o.key) for o in objs]
         )
