ef796a45b21ec6ce4e4ebb08315f2b66a771b228
Adam Green <adam.green@adgefficiency.com>
2019-08-30 20:40:44 +0200

    sync

--- worldmodels/dataset/sample_latent_stats.py
+++ worldmodels/dataset/sample_latent_stats.py
@@ -7,49 +7,38 @@ loads VAE
 
 get mu and theta (from sampled obs)
 
-sample a different latent vector each time
-
-used after VAE training, before memory training
-
+this allows us to sample a different latent vector each time
 """
+
 import argparse
 from os.path import join
-from os import makedirs, environ
 
-import numpy as np
 import tensorflow as tf
 
-from worldmodels.dataset.upload_to_s3 import S3
-from worldmodels.params import vae_params
+from worldmodels.params import vae_params, results_dir
 from worldmodels.dataset.tf_records import encode_floats, batch_episodes, parse_random_rollouts
 from worldmodels.vision.vae import VAE
-
-
-home = environ['HOME']
-results_dir = join(home, 'world-models-experiments', 'latent-stats')
-makedirs(results_dir, exist_ok=True)
+from worldmodels.utils import list_records, make_directories
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--episode_start', default=0, nargs='?', type=int)
     parser.add_argument('--episodes', default=10000, nargs='?', type=int)
+    parser.add_argument('--data', default='local', nargs='?')
     args = parser.parse_args()
     print(args)
 
+    records = list_records('random-rollouts', 'episode', args.data)
+    make_directories('latent-stats')
+    results_dir = join(results_dir, 'latent-stats')
+
     episode_start = args.episode_start
     episodes = args.episodes
-
-    model = VAE(**vae_params)
-
-    print('setting env variables for AWS')
-    environ["AWS_REGION"] = "eu-central-1"
-    environ["AWS_LOG_LEVEL"] = "3"
-
-    s3 = S3()
-    records = s3.list_all_objects('random-rollouts')
     records = records[episode_start: episode_start + episodes]
     dataset = batch_episodes(parse_random_rollouts, records, episode_length=1000)
+
+    model = VAE(**vae_params)
 
     for episode in range(episode_start, episode_start + episodes):
         print('processing episode {}'.format(episode))
@@ -61,7 +50,6 @@ if __name__ == '__main__':
         print('saving to {}'.format(path))
         with tf.io.TFRecordWriter(path) as writer:
             encoded = encode_floats({
-                # 'observation': obs.numpy(),
                 'action': act.numpy(),
                 'mu': mu.numpy(),
                 'logvar': logvar.numpy(),
